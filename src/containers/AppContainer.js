9ec2bcf37bd641eb45149a4d446121c773595403
Jack Willis-Craig <jack.willis-craig@hotmail.com>
2016-09-19 23:27:34 +1000

    General updates.

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,22 +1,26 @@
 import React, { Component, PropTypes } from 'react'
 import { Router } from 'react-router'
 import { Provider } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class AppContainer extends Component {
   static propTypes = {
     history: PropTypes.object.isRequired,
     routes: PropTypes.object.isRequired,
+    routerKey: PropTypes.number,
     store: PropTypes.object.isRequired
   }
 
-  render () {
-    const { history, routes, store } = this.props
+  render() {
+    const { history, routes, routerKey, store } = this.props
 
     return (
       <Provider store={store}>
-        <div style={{ height: '100%' }}>
-          <Router history={history} children={routes} />
-        </div>
+        <MuiThemeProvider>
+          <div style={{ height: '100%' }}>
+            <Router history={history} children={routes} key={routerKey} />
+          </div>
+        </MuiThemeProvider>
       </Provider>
     )
   }
