dce9cb6459a36045a2b2c429b47d151f75b0f549
Jack Willis-Craig <jack.willis-craig@hotmail.com>
2016-09-20 02:43:41 +1000

    Added searching to the deals list.

--- src/routes/Deals/containers/DealsContainer.js
+++ src/routes/Deals/containers/DealsContainer.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import TextField from 'material-ui/TextField'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 
 import Deals from '../components/Deals'
+import { IconClose, IconSearch } from 'components/Icons'
 import { fetchDeals } from 'store/deals/actions'
 
+import classes from './DealsContainer.scss'
 import strings from './DealsContainer.strings.js'
 
 export default class DealsContainer extends React.Component {
@@ -16,7 +19,14 @@ export default class DealsContainer extends React.Component {
    */
   constructor(props) {
     super(props)
+
+    this.state = {
+      filteredDeals: props.deals.deals || [],
+      searching: false
+    }
     this._onDealClick = ::this._onDealClick
+    this._onFilterChange = ::this._onFilterChange
+    this._toggleSearch = ::this._toggleSearch
   }
 
   /**
@@ -28,8 +38,27 @@ export default class DealsContainer extends React.Component {
       fetchDeals()
     }
     layout.setHeader({
+      contextualOptions: [
+        <IconSearch
+          onClick={this._toggleSearch}
+        />
+      ],
       title: strings.title
     })
+  }
+
+  /**
+   * Updates the deals array when new deals are received.
+   *
+   * @param nextProps
+   */
+  componentWillReceiveProps(nextProps) {
+    const { deals } = nextProps
+    if (this.props.deals.deals === null && deals.deals) {
+      this.setState({
+        filteredDeals: deals.deals
+      })
+    }
   }
 
   /**
@@ -43,23 +72,80 @@ export default class DealsContainer extends React.Component {
   }
 
   /**
+   * Toggles the search bar.
+   *
+   * @private
+   */
+  _toggleSearch() {
+    this.setState({
+      searching: !this.state.searching
+    }, () => {
+      let search =
+        <IconSearch
+          onClick={this._toggleSearch}
+        />
+
+      if (this.state.searching) {
+        search =
+          <div>
+            <div className={classes.search}>
+              <TextField
+                name='search'
+                onChange={this._onFilterChange}
+                ref={c => c !== null && c.focus()}
+              />
+              <IconClose
+                onClick={this._toggleSearch}
+              />
+            </div>
+          </div>
+      }
+
+      this.props.layout.updateHeader({
+        contextualOptions: [search]
+      })
+    })
+  }
+
+  /**
+   * Updates the deal filter.
+   *
+   * @param e
+   * @private
+   */
+  _onFilterChange(e) {
+    const filter = e.target.value
+    const deals = this.props.deals.deals.filter(deal =>
+      Object.keys(deal).some(key =>
+        typeof deal[key] === 'string' && (new RegExp(filter, 'i').test(deal[key]))
+      )
+    )
+    this.setState({
+      filteredDeals: deals
+    })
+  }
+
+  /**
    * Renders the Navigation Drawer.
    *
    * @returns {*}
    */
   render() {
     const { deals } = this.props
+    const { filteredDeals } = this.state
 
     if (deals.loading || !deals.deals) {
       return null
     }
 
     return (
-      <Deals
-        {...this.props}
-        deals={deals.deals}
-        onDealClick={this._onDealClick}
-      />
+      <div>
+        <Deals
+          {...this.props}
+          deals={filteredDeals}
+          onDealClick={this._onDealClick}
+        />
+      </div>
     )
   }
 }
