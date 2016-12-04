a417683f32c02d62195f11ab4f0db528f2ef596b
Alex Berriman <aberriman@formcorp.com.au>
2016-12-04 18:06:22 +1100

    Add Google Maps preview

--- src/components/SearchMap/SearchMap.js
+++ src/components/SearchMap/SearchMap.js
@@ -1,14 +1,28 @@
 import React from 'react'
 
+import GoogleMap from 'google-map-react';
+
 import classes from './SearchMap.scss'
 
-export default class SearchMap extends React.Component {
-  render() {
 
+export default class SearchMap extends React.Component {
+  static defaultProps = {
+    center: [59.938043, 30.337157],
+    zoom: 9,
+    greatPlaceCoords: {lat: 59.724465, lng: 30.080121}
+  }
+
+  render() {
     return (
       <div className={classes.search}>
-        search map
+
       </div>
     )
   }
+}
+
+SearchMap.propTypes = {
+  center: React.PropTypes.array,
+  zoom: React.PropTypes.number,
+  greatPlaceCoords: React.PropTypes.any
 }
