2cddfd7b4a6cbaa19e03ad244fef2a0a5e195f56
Alex Berriman <aberriman@formcorp.com.au>
2017-01-08 16:46:52 +1100

    feat: add google maps to search result

--- src/src/components/SearchMap/SearchMap.js
+++ src/src/components/SearchMap/SearchMap.js
@@ -1,21 +1,54 @@
 import React from 'react'
 
-import GoogleMap from 'google-map-react';
+import {
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps"
+
+import withScriptjs from "react-google-maps/lib/async/withScriptjs";
+import FontAwesome from "react-fontawesome"
 
 import classes from './SearchMap.scss'
 
+const SearchResultMap = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      ref={props.onMapLoad}
+      defaultZoom={3}
+      defaultCenter={{lat: -25.363882, lng: 131.044922}}
+      onClick={props.onMapClick}
+    >
+
+    </GoogleMap>
+  ))
+)
 
 export default class SearchMap extends React.Component {
-  static defaultProps = {
-    center: [59.938043, 30.337157],
-    zoom: 9,
-    greatPlaceCoords: {lat: 59.724465, lng: 30.080121}
+  _lorem() {
+    console.log('lorem ipsum')
   }
 
   render() {
     return (
       <div className={classes.search}>
-
+        <SearchResultMap
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp"
+          loadingElement={
+            <div style={{height: `100%`}}>
+              <FontAwesome name='rocket' />
+            </div>
+          }
+          containerElement={
+            <div style={{height: `100%`}}/>
+          }
+          mapElement={
+            <div style={{height: `100%`}}/>
+          }
+          onMapLoad={this._lorem}
+          onMapClick={this._lorem}
+          onMarkerRightClick={this._lorem}
+        />
       </div>
     )
   }
